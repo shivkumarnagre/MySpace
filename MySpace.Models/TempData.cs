201ee781d283e96f3514c00aa26fbd66767a219f
Shivkumar Nagre <shivkumarnagre.sk@gmail.com>
2021-12-07 15:06:52 +0530

    First

---
+++ MySpace.Models/TempData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MySpace.Models
+{
+    public class TempData
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+    }
+}
