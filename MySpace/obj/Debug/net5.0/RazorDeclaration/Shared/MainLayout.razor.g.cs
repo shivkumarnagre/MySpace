df38058e84c9df778e62d099d1f7d6a751061fa7
Shivkumar Nagre <shivkumarnagre.sk@gmail.com>
2021-12-07 13:53:49 +0530

    First Commit

--- MySpace/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ MySpace/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,84 +13,84 @@ namespace MySpace.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 1 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 2 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 3 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 4 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 5 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 6 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 7 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 8 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 9 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using MySpace;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 10 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using MySpace.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\_Imports.razor"
+#line 11 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\Shared\MainLayout.razor"
+#line 2 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\Shared\MainLayout.razor"
 using MySpace.Models;
 
 #line default
@@ -104,7 +104,7 @@ using MySpace.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 31 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_v3.0\MySpace\Shared\MainLayout.razor"
+#line 31 "C:\Users\Shiv\OneDrive\Desktop\New folder\MySpace\MySpace\Shared\MainLayout.razor"
        
     public string stringValue { get; set; }
 
