fbbdfbf982a237d25cc44535304d21cf953a5fba
Shivkumar Nagre <shivkumarnagre.sk@gmail.com>
2021-12-18 21:53:24 +0530

    Database File

---
+++ dbo.PrsnlSpace.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[PrsnlSpace] (
+    [fname]     VARCHAR (50) NULL,
+    [lname]     VARCHAR (50) NULL,
+    [msg]       VARCHAR (50) NULL,
+    [email]     VARCHAR (50) NULL,
+    [date]      DATE         NULL,
+    [time]      TIME (7)     NULL,
+    [id]        INT          NOT NULL,
+    [spacename] VARCHAR (50) NULL,
+    [spacedesc] VARCHAR (50) NULL,
+    CONSTRAINT [PK_PrsnlSpace] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
