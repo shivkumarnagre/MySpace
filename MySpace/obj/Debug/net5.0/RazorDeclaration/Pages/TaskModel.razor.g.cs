f42119a22eec02baf187db5efaa9c2ff691dc295
Shivkumar Nagre <shivkumarnagre.sk@gmail.com>
2021-12-19 12:24:28 +0530

    Initial

--- MySpace/obj/Debug/net5.0/RazorDeclaration/Pages/TaskModel.razor.g.cs
+++ MySpace/obj/Debug/net5.0/RazorDeclaration/Pages/TaskModel.razor.g.cs
@@ -13,84 +13,84 @@ namespace MySpace.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 1 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 2 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 3 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 4 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 5 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 6 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 7 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 8 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 9 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using MySpace;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 10 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using MySpace.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\_Imports.razor"
+#line 11 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\Pages\TaskModel.razor"
+#line 2 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\Pages\TaskModel.razor"
 using MySpace.Models;
 
 #line default
@@ -104,7 +104,7 @@ using MySpace.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 92 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\Pages\TaskModel.razor"
+#line 92 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\Pages\TaskModel.razor"
        
     [CascadingParameter] MudDialogInstance MudDialog { get; set; }
 
@@ -119,7 +119,7 @@ using MySpace.Models;
 #line hidden
 #nullable disable
 #nullable restore
-#line 106 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace\MySpace\MySpace\Pages\TaskModel.razor"
+#line 106 "C:\Users\Shiv\OneDrive\Desktop\TeachBit\MySpace_master13_12\MySpace\Pages\TaskModel.razor"
            
 
     public class TodoItem { public string? Title { get; set; } }
